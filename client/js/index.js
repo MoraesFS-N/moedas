139bce64b1742075e4578695440d8cf7f361499a
Moraes Felicio da Silva Neto <moraes.neto@mercovia-sa.com>
2020-07-13 18:55:41 -0300

    solução alternativa

--- client/js/index.js
+++ client/js/index.js
@@ -1,8 +1,35 @@
-var campos = [
+var campos = [    //Pegando valores de um form
     document.querySelector('#data'),
     document.querySelector('#quantidade'),
     document.querySelector('#valor')
 ];
 
-document.querySelector('.form').addEventListener('submit');
+var tbody = document.querySelector('table tbody');
+// Adicionando ouvidor de eventos
+document.querySelector('.form').addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    // criando linha na tabela
+    var tr = document.createElement('tr');
+
+    campos.forEach(function(campo){
+        var td = document.createElement('td');
+        td.textContent = campo.value;           //
+        tr.appendChild(td);                     //
+    });
+
+    var tdVolume = document.createElement('td');
+    // com a multiplicação dos campos correspondentes na posição 1 = quantidade e posição 2 = quantidade
+    tdVolume.textContent = campos[1].value = campos[2].value;
+    tr.appendChild(tdVolume);
+
+    tbody.appendChild(tr);
+
+    campos[0].value = '';
+    campos[1].value = 1;
+    campos[2].value = 0;
+
+    campos[1].focus();
+ 
+});
 
